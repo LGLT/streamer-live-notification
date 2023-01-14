a987fd2ae5aeecbe8fb1b49bd8e33e69acf4a6c0
LGLT <leonardo.lopez.jobs@gmail.com>
2023-01-14 14:45:41 -0600

    Updated version. Added: Call to server to get dinamically token

--- public/background.js
+++ public/background.js
@@ -11,58 +11,62 @@ function getStreamers() {
 
 
 function checkStreamerStatus() {
-
-  getStreamers().then(streamers => {
-    for (let i = 0; i < streamers.length; i++) {
-      const streamer = streamers[i];
-      const twitchAPIUrl = `https://api.twitch.tv/helix/streams?user_login=${streamer.name}`;
-      const headers = {
-        'Client-ID': 'zx08m72z3tj0ql8piccly7rkgum367',
-        'Authorization': 'Bearer ynd117is6g6iivi15g1d1ml6de8gmg',
-      };
-      
-      let notificationRegister = notificationsHandler.find( (notif) => notif.name === streamer.name );
-      if( !notificationRegister ) {
-        notificationsHandler.push({
-          name: streamer.name,
-          state: 'offline', 
-        })
-        notificationRegister = Array.isArray(notificationsHandler.slice(-1)) ? notificationsHandler.slice(-1)[0] : notificationsHandler.slice(-1);
-      }
-
-      fetch(twitchAPIUrl, {headers: headers})
-      .then(response => response.json())
-      .then(data => {
-        if (data.data.length > 0 && notificationRegister.state == 'offline') {
-          chrome.notifications.create({
-            type: 'basic',
-            iconUrl: 'icon128.png',
-            title: 'Streamer Live Notification',
-            message: `${streamer.name} is live now on 'twitch.tv/${streamer.name}'`,
-            buttons: [
-              { title: 'Open on Twitch' },
-            ],
-          }, function(notificationId) {
-            notificationRegister.id = notificationId;
+  const extensionServer = 'https://streamers-live-notif-server.herokuapp.com/twitch-app-auth';
+  fetch(extensionServer) 
+    .then(extensionServerResponse => extensionServerResponse.json())
+    .then( authData => {
+      getStreamers().then(streamers => {
+        for (let i = 0; i < streamers.length; i++) {
+          const streamer = streamers[i];
+          const twitchAPIUrl = `https://api.twitch.tv/helix/streams?user_login=${streamer.name}`;
+          const headers = {
+            'Client-ID': 'zx08m72z3tj0ql8piccly7rkgum367',
+            'Authorization': `Bearer ${authData.access_token}`,
+          };
+          
+          let notificationRegister = notificationsHandler.find( (notif) => notif.name === streamer.name );
+          if( !notificationRegister ) {
+            notificationsHandler.push({
+              name: streamer.name,
+              state: 'offline', 
+            })
+            notificationRegister = Array.isArray(notificationsHandler.slice(-1)) ? notificationsHandler.slice(-1)[0] : notificationsHandler.slice(-1);
+          }
+    
+          fetch(twitchAPIUrl, {headers: headers})
+          .then(response => response.json())
+          .then(data => {
+            if (data.data.length > 0 && notificationRegister.state == 'offline') {
+              chrome.notifications.create({
+                type: 'basic',
+                iconUrl: 'icon128.png',
+                title: 'Streamer Live Notification',
+                message: `${streamer.name} is live now on 'twitch.tv/${streamer.name}'`,
+                buttons: [
+                  { title: 'Open on Twitch' },
+                ],
+              }, function(notificationId) {
+                notificationRegister.id = notificationId;
+              });
+    
+              notificationRegister.state = 'online';
+              streamers[i].state = 'online';
+              chrome.storage.local.set({ streamers });
+            }
+          })
+          .catch(error => {
+            notificationRegister.state = 'offline'
+            streamers[i].state = 'offline';
+            chrome.storage.local.set({ streamers });
           });
-
-          notificationRegister.state = 'online';
-          streamers[i].state = 'online';
-          chrome.storage.local.set({ streamers });
+    
+          if(notificationRegister.state === 'online' && streamers[i].state === 'offline') {
+            streamers[i].state = 'online'
+            chrome.storage.local.set({ streamers });
+          }
         }
-      })
-      .catch(error => {
-        notificationRegister.state = 'offline'
-        streamers[i].state = 'offline';
-        chrome.storage.local.set({ streamers });
-      });
-
-      if(notificationRegister.state === 'online' && streamers[i].state === 'offline') {
-        streamers[i].state = 'online'
-        chrome.storage.local.set({ streamers });
-      }
-
-  }
+    })
+    .catch(err => console.log(err))
   });
 }
 
